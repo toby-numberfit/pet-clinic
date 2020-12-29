b2287b04228f1e4e3e590a28708eb350434b130f
Toby <72808353+toby-numberfit@users.noreply.github.com>
2020-12-29 02:43:46 +0000

    Fix #1: add horse names in database (#4)
    * Fix #1: add horse names in database
    
    * Added the missing closing parenthesis ')' for 'Jolly Jumper'.
    
    * Fixed syntax issue for 'Jeff Black' entry in 'owner'.
    Fixed a syntax issue, which included newlines, that malformed the `INSERT INTO owners` query for 'Jeff Black'.
    
    * Renamed 'Jolly Jumper' to 'Silver Blaze'.

--- src/main/resources/db/hsqldb/data.sql
+++ src/main/resources/db/hsqldb/data.sql
@@ -30,10 +30,7 @@ INSERT INTO owners VALUES (3, 'Eduardo', 'Rodriquez', '2693 Commerce St.', 'McFa
 INSERT INTO owners VALUES (4, 'Harold', 'Davis', '563 Friendly St.', 'Windsor', '6085553198');
 INSERT INTO owners VALUES (5, 'Peter', 'McTavish', '2387 S. Fair Way', 'Madison', '6085552765');
 INSERT INTO owners VALUES (6, 'Jean', 'Coleman', '105 N. Lake St.', 'Monona', '6085552654');
-INSE
-data.sql
-  
-RT INTO owners VALUES (7, 'Jeff', 'Black', '1450 Oak Blvd.', 'Monona', '6085555387');
+INSERT INTO owners VALUES (7, 'Jeff', 'Black', '1450 Oak Blvd.', 'Monona', '6085555387');
 INSERT INTO owners VALUES (8, 'Maria', 'Escobito', '345 Maple St.', 'Madison', '6085557683');
 INSERT INTO owners VALUES (9, 'David', 'Schroeder', '2749 Blackhawk Trail', 'Madison', '6085559435');
 INSERT INTO owners VALUES (10, 'Carlos', 'Estaban', '2335 Independence La.', 'Waunakee', '6085555487');
@@ -51,6 +48,8 @@ INSERT INTO pets VALUES (10, 'Mulligan', '2007-02-24', 2, 8);
 INSERT INTO pets VALUES (11, 'Freddy', '2010-03-09', 5, 9);
 INSERT INTO pets VALUES (12, 'Lucky', '2010-06-24', 2, 10);
 INSERT INTO pets VALUES (13, 'Sly', '2012-06-08', 1, 10);
+INSERT INTO pets VALUES (14, 'Silver Blaze', '2012-09-20', 7, 5);
+INSERT INTO pets VALUES (15, 'Flycka', '2012-07-14', 7, 9);
 
 INSERT INTO visits VALUES (1, 7, '2013-01-01', 'rabies shot');
 INSERT INTO visits VALUES (2, 8, '2013-01-02', 'rabies shot');
